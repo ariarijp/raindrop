24f44fa302815108c33928e6d791ca6b86703dd2
Takuya Arita <takuya.arita@gmail.com>
2015-04-26 23:02:45 +0900

    Fix build script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+go get code.google.com/p/goauth2/oauth
+go get github.com/digitalocean/godo
+
 go build -o bin/create-droplets create-droplets.go
 go build -o bin/destroy-all-droplets destroy-all-droplets.go
 go build -o bin/list-droplet-addresses list-droplet-addresses.go
